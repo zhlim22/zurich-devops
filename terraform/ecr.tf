643091ba07f2ea14eb0529da8d53a3a8ba4cb103
Lim Zhan Hong <zhlim22@gmail.com>
2025-05-25 02:10:12 +0800

    Revert changes

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,38 +6,4 @@ resource "aws_ecr_repository" "ecr" {
   image_scanning_configuration {
     scan_on_push = true
   }
-
-  # data "aws_iam_policy_document" "ecr_policy" {
-  #   statement {
-  #     sid    = "ECR"
-  #     effect = "Allow"
-  #
-  #     principals {
-  #       identifiers = ["arn:aws:iam::479699168399:user/terraform-user"]
-  #       type        = "AWS"
-  #     }
-  #
-  #     actions = [
-  #       "ecr:GetDownloadUrlForLayer",
-  #       "ecr:BatchGetImage",
-  #       "ecr:BatchCheckLayerAvailability",
-  #       "ecr:PutImage",
-  #       "ecr:InitiateLayerUpload",
-  #       "ecr:UploadLayerPart",
-  #       "ecr:CompleteLayerUpload",
-  #       "ecr:DescribeRepositories",
-  #       "ecr:GetRepositoryPolicy",
-  #       "ecr:ListImages",
-  #       "ecr:DeleteRepository",
-  #       "ecr:BatchDeleteImage",
-  #       "ecr:SetRepositoryPolicy",
-  #       "ecr:DeleteRepositoryPolicy",
-  #     ]
-  #   }
-  # }
-  #
-  # resource "aws_ecr_repository_policy" "private_policy" {
-  #   repository = aws_ecr_repository.ecr.name
-  #   policy     = data.aws_iam_policy_document.ecr_policy.json
-  # }
 }
